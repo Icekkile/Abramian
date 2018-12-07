e290110ad9fff4935872e54b1cd5ff9186043f91
icekkile <iceman.ryt@gmail.com>
2018-12-07 22:01:14 +0200

    Program.cs was modyfied

--- boolean/Program.cs
+++ boolean/Program.cs
@@ -12,7 +12,7 @@ namespace boolean
 
         static void Main(string[] args)
         {
-
+            p(Math.Abs (-1));
         }
 
         static void ex1(int A)
@@ -100,7 +100,7 @@ namespace boolean
 
         static void ex16 (int A)
         {
-            if (A < 100 || A % 2 != 1) p("Yes");
+            if (A < 100 && A % 2 != 1) p("Yes");
             else p("NOPE");
         }
 
@@ -116,6 +116,11 @@ namespace boolean
             else p("NOPE");
         }
 
+        static void ex19 ()
+        {
+
+        }
+
         static void ex20 (int A)
         {
             if (!(A >= 100 && A < 1000)) return;
@@ -125,5 +130,92 @@ namespace boolean
             if (s1 != s2 && s2 != s3 && s3 != s1) p("Yes");
             else p("Nope");
         }
+
+        static void ex21 (int A)
+        {
+            int s1 = A / 100;
+            int s2 = (A % 100) / 10;
+            int s3 = (A % 100) % 10;
+            if (s1 < s2 < s3) p("Yes");
+            else p("Nope");
+        }
+
+        static void ex22(int A)
+        {
+            int s1 = A / 100;
+            int s2 = (A % 100) / 10;
+            int s3 = (A % 100) % 10;
+            if (s1 < s2 < s3) p("Yes from lowest to the largest");
+            else if (s1 > s2 > s3) p("Yes from the largest to lowest");
+            else p("Nope");
+        }
+
+        static void ex23(int A)
+        {
+            int s0 = A / 1000;
+            int s1 = (A % 1000) / 100;
+            int s2 = ((A % 1000) % 100) / 10;
+            int s3 = ((A % 1000) % 100) % 10;
+            if ((s3 * 1000) + (s2 * 100) + (s1 * 10) + s0 == A) p("Yes");
+            else p("Nope");
+        }
+
+        // if length
+        static void ex30 (int a, int b, int c)
+        {
+            if (a == b && b == c) p("Yes");
+            else p("Nope");
+        }
+        // if length
+        static void ex31 (int a, int b, int c)
+        {
+            if (a == b || b == c || a == c) p("Yes");
+            else p("Nope");
+        }
+        // if length
+        static void ex32(int a, int b, int c)
+        {
+            if (a * a + b * b == c * c) p("Yeeeeep");
+            else p("Nope");
+        }
+        //if degreeses
+        static void ex33 (int a, int b, int c)
+        {
+            if (a + b + c <= 180) p("Yes");
+            else p("Nope");
+        }
+
+        static void ex34 (int x, int y)
+        {
+            if ((x % 2 != 1 && y % 2 != 1) || (x % 2 == 1 && y % 2 == 1)) p("black");
+            else p("white");
+        }
+
+        static void ex35(int x, int y, int x2, int y2)
+        {
+            bool a, b;
+            if ((x % 2 != 1 && y % 2 != 1) || (x % 2 == 1 && y % 2 == 1)) { p("black"); a = true; }
+            if ((x2 % 2 != 1 && y2 % 2 != 1) || (x2 % 2 == 1 && y2 % 2 == 1)) { p("black"); a = true; }
+            if (a == b) p("Yes");
+        }
+
+        static void ex36 (int x, int y, int x2, int y2)
+        {
+            if (x == x2 || y == y2) p("Yes");
+            else p("Nope");
+        }
+
+        static void ex37(int x, int y, int x2, int y2)
+        {
+
+        }
+
+        static void ex38 (int x, int y, int x2, int y2)
+        {
+            if (x == y && x2 == y2) p("Yes");
+            else p("Nope");
+        }
+
+
     }
 }
